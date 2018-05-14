27a1301e0ff02de64bf274e396c2357cca8f755d
hkisthebest <hkisthebest@gmail.com>
2018-05-15 01:09:24 +0800

    1

--- app/src/main/java/com/example/user/troncalendar/MainActivity.java
+++ app/src/main/java/com/example/user/troncalendar/MainActivity.java
@@ -7,7 +7,7 @@ import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+     DatabaseHelper mydb;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,6 +21,7 @@ public class MainActivity extends AppCompatActivity {
                 startNextPage();
             }
         });
+        mydb = new DatabaseHelper(this);
     }
 
     private void startNextPage(){
