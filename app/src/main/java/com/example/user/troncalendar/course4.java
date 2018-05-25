3a5cc55ad954dfb0ae0df0476b9631a006338aba
Erik <hkisthebest@gmail.com>
2018-05-25 23:08:54 +0800

    profile and cardview

---
+++ app/src/main/java/com/example/user/troncalendar/course4.java
@@ -0,0 +1,13 @@
+package com.example.user.troncalendar;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class course4 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.course4);
+    }
+}
