972025f08e6eedff78e51cb5c7e4dffeba985237
HsuWilliam <hsu870422@gmail.com>
2018-05-28 00:27:33 +0800

    Alert dialog

--- app/src/main/java/com/example/user/troncalendar/MainActivity.java
+++ app/src/main/java/com/example/user/troncalendar/MainActivity.java
@@ -1,33 +1,93 @@
 package com.example.user.troncalendar;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
 //test
 public class MainActivity extends AppCompatActivity {
-     DatabaseHelper mydb;
+    private EditText studentnumber;
+    private EditText pass;
+    private Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        // test merges8787
         Button button = (Button) findViewById(R.id.button);
-
+        studentnumber = (EditText) this.findViewById(R.id.studentnumber);
+        pass = (EditText) this.findViewById(R.id.password);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startNextPage();
+                String[] account = {"405402340", "405402508","405402223","405402314","405402089"};
+                String[] password = {"405402340", "405402508","405402223","405402314","405402089"};
+                for (int i = 0; i < account.length; i++) {
+                    if (studentnumber.getText().toString().equals(account[i]) && pass.getText().toString().equals(password[i])) {
+                        new AlertDialog.Builder(MainActivity.this).setTitle("登入訊息").setIcon(R.mipmap.ic_launcher).setMessage("登入成功").setPositiveButton("確認"
+                                , new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialog, int which) {
+                                        Intent intent = new Intent();
+                                        intent.setClass(MainActivity.this,course.class);
+                                        startActivity(intent);
+                                    }
+                                }).show();
+                        break;
+                    } else {
+                        new AlertDialog.Builder(MainActivity.this).setTitle("登入訊息").setIcon(R.mipmap.ic_launcher).setMessage("密碼錯誤").setPositiveButton("返回"
+                                , new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialog, int which) {
+                                        Intent intent = new Intent();
+                                        intent.setClass(MainActivity.this,MainActivity.class);
+                                        startActivity(intent);
+                                    }
+                                }).show();
+
+                    }
+                }
             }
         });
-        mydb = new DatabaseHelper(this);
-    }
-//this is a test
-    private void startNextPage(){
-        Intent intent = new Intent();
-        intent.setClass(this , course.class);
-        startActivity(intent);
     }
 
+
+
+
+
+
+    // };
+
+    // test merges8787
+
+
+    // @Override
+    //    public void onClick(View v) {
+    //          startNextPage();
+    //      }
+    //  });
+
+    // }
+
+//this is a test
+//   private void startNextPage(){
+    //    Intent intent = new Intent();
+    //   intent.setClass(this , course.class);
+    //   startActivity(intent);
+    //   }
+
+    //}
+  // private void startNewPage(){
+      //  Intent intent = new Intent();
+     //   intent.setClass(this , course.class);
+     //   startActivity(intent);
+ //   }
 }
