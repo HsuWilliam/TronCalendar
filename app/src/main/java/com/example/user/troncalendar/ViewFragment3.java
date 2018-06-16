7bce010ea4976b942b5a8f4592b0e15865d3dbbb
HsuWilliam <hsu870422@gmail.com>
2018-06-16 23:24:01 +0800

    update

--- app/src/main/java/com/example/user/troncalendar/ViewFragment3.java
+++ app/src/main/java/com/example/user/troncalendar/ViewFragment3.java
@@ -1,47 +1,74 @@
 package com.example.user.troncalendar;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.squareup.picasso.Picasso;
+
 public class ViewFragment3 extends Fragment {
+    private RecyclerView mActivity3;
+    private DatabaseReference mDatabase;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-    //    TextView textView2 = (TextView) rootView.findViewById(R.id.txt_label2);
-   //     TextView textView3 = (TextView) rootView.findViewById(R.id.txt_label3);
-      //  TextView textView5 = (TextView) rootView.findViewById(R.id.txt_label5);
-
-     //   textView2.setText("名稱：幹部面試");
-     //   textView3.setText("人數：500");
-    //    textView5.setText("時間：");
-
-      //  TextView textView6 = (TextView) rootView.findViewById(R.id.txt_label6);
-     //   TextView textView7 = (TextView) rootView.findViewById(R.id.txt_label7);
-     //   TextView textView9 = (TextView) rootView.findViewById(R.id.txt_label9);
-
-      //  textView6.setText("名稱：幹部面試");
-     //   textView7.setText("人數：500");
-    //    textView9.setText("時間：");
-
-     //   TextView textView10 = (TextView) rootView.findViewById(R.id.txt_label10);
-     //   TextView textView11 = (TextView) rootView.findViewById(R.id.txt_label11);
-     //   TextView textView12 = (TextView) rootView.findViewById(R.id.txt_label12);
-
-     //   textView10.setText("名稱：幹部面試");
-     //   textView11.setText("人數：500");
-     //   textView12.setText("時間：");
-
-     //   TextView textView13 = (TextView) rootView.findViewById(R.id.txt_label13);
-       // TextView textView14 = (TextView) rootView.findViewById(R.id.txt_label14);
-       // TextView textView15 = (TextView) rootView.findViewById(R.id.txt_label15);
-
-      //  textView13.setText("名稱：幹部面試");
-     //   textView14.setText("人數：500");
-    //    textView15.setText("時間：");
+        mDatabase = FirebaseDatabase.getInstance().getReference("社團");
+        mDatabase.keepSynced(true);
+        mActivity3 = (RecyclerView) rootView.findViewById(R.id.recyclerview3);
+        mActivity3.setLayoutManager(new LinearLayoutManager(getContext()));
+        mActivity3.setHasFixedSize(true);
         return rootView;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        FirebaseRecyclerAdapter<activitylist3, activityViewHolder3> firebaseRecyclerAdapter = new FirebaseRecyclerAdapter<activitylist3, activityViewHolder3>
+                (activitylist3.class,R.layout.activity3_row,activityViewHolder3.class,mDatabase) {
+            @Override
+            protected void populateViewHolder(activityViewHolder3 viewHolder, activitylist3 model, int position) {
+                viewHolder.setTitle(model.getTitle());
+                viewHolder.setClub(model.getClub());
+                viewHolder.setTime(model.getTime());
+                viewHolder.setImage(getContext().getApplicationContext(),model.getImage());
+            }
+        };
+        mActivity3.setAdapter(firebaseRecyclerAdapter);
+    }
+    public static class activityViewHolder3 extends RecyclerView.ViewHolder{
+        View mView;
+        public activityViewHolder3(View itemView){
+            super(itemView);
+            mView= itemView;
+        }
+
+        public void setTitle(String title){
+            TextView post_activity3 = (TextView)mView.findViewById(R.id.post_activity3);
+            post_activity3.setText(title);
+        }
+
+        public void setClub(String club){
+            TextView post_activityclub = (TextView)mView.findViewById(R.id.post_activityclub);
+            post_activityclub.setText(club);
+        }
+        public void setTime(String time){
+            TextView post_activitytime = (TextView)mView.findViewById(R.id.post_activitytime);
+            post_activitytime.setText(time);
+        }
+        public void setImage(Context ctx, String image){
+            ImageView post_image3 = (ImageView)mView.findViewById(R.id.post_image3);
+            Picasso.with(ctx).load(image).into(post_image3);
+        }
     }
 }
