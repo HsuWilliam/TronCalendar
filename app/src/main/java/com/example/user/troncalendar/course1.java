2bd7fcfc75c650f67c8274a33bdda00ae1010543
kevinsong0302 <zse125252626@gmail.com>
2018-06-22 02:56:03 +0800

    ..

--- app/src/main/java/com/example/user/troncalendar/course1.java
+++ app/src/main/java/com/example/user/troncalendar/course1.java
@@ -1,20 +1,18 @@
 package com.example.user.troncalendar;
 
 import android.content.Intent;
-import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.TextView;
 
-import org.w3c.dom.Text;
+public class course1 extends AppCompatActivity {
 
-public class course2 extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.course2);
+        setContentView(R.layout.course1);
+
         ImageView imageView3 = findViewById(R.id.imageView3);
         imageView3.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -22,18 +20,20 @@ public class course2 extends AppCompatActivity {
                 startNextPage();
             }
         });
-        ImageView imageView4 = findViewById(R.id.imageView4);
-        imageView4.setOnClickListener(new View.OnClickListener() {
+
+        ImageView imageView5 = findViewById(R.id.imageView5);
+        imageView5.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startNextPage2();
+                startNextPage();
             }
         });
-        //course
-        TextView textView4 = findViewById(R.id.textView4);
-        String title = "課程";
-        textView4.setText(title);
+
+
     }
+
+
+
     private void startNextPage(){
         Intent intent = new Intent();
         intent.setClass(this , coursepost1.class);
@@ -44,5 +44,6 @@ public class course2 extends AppCompatActivity {
         Intent intent = new Intent();
         intent.setClass(this , homework1.class);
         startActivity(intent);
-    }
 }
+}
+
